a2c98597df1bb6d1238142c9f082e3414526e7ec
Vandan Patel <vandan.patel@tstmedia.com>
2013-01-27 14:36:02 -0600

    Validations added to Post

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '837cdce096e27986beb44c4811dd578e1cbcdb271f5fa3f6c5b7ad5d961f122199b21696c95ce0210df6177ce01dbe48d860e09a8b1e2f6f5ad3f44fddaac8c8'
